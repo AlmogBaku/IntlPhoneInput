6562e3792a51ab1fcfd3a64c02c2b49490fe9b0f
AlmogBaku <almog.baku@gmail.com>
2015-05-19 01:06:45 +0300

    add splash as login-session switcher

--- app/src/main/java/net/rimoto/wheninroam/activity/SplashActivity.java
+++ app/src/main/java/net/rimoto/wheninroam/activity/SplashActivity.java
@@ -1,11 +1,27 @@
 package net.rimoto.wheninroam.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
 import net.rimoto.wheninroam.R;
+import net.rimoto.wheninroam.utils.RimotoCore.Session;
 
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_splash)
 public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent;
+        if(Session.getCurrentAccessToken(this)==null) {
+            intent = new Intent(this, LoginActivity_.class);
+        } else {
+            intent = new Intent(this, WizardActivity_.class);
+        }
+        startActivity(intent);
+        finish();
+    }
 }
