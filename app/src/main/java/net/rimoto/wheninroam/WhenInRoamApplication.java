0c3867938a871dbdaad1d4959249e5c002733524
AlmogBaku <almog.baku@gmail.com>
2015-05-18 18:47:37 +0300

    use the correct url

--- app/src/main/java/net/rimoto/wheninroam/WhenInRoamApplication.java
+++ app/src/main/java/net/rimoto/wheninroam/WhenInRoamApplication.java
@@ -10,12 +10,13 @@ import org.androidannotations.annotations.EApplication;
 @EApplication
 public class WhenInRoamApplication extends Application {
     private final String rimotoClientID = "3_4xs1w1ynms08w8sc0w0k8k40kksccwwwwo8scso44c08c4goo4";
+    private final String rimoto_authURI = "http://wheninroam.parseapp.com/inbound_rimoto_auth";
 
     @Override
     public void onCreate() {
         super.onCreate();
         try {
-            RimotoCore.init(rimotoClientID);
+            RimotoCore.init(rimotoClientID, rimoto_authURI);
             RimotoCore.setAuthType("token");
         } catch (RimotoException ignored) {}
     }
