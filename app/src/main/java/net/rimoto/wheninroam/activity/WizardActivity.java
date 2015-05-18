0479f296e0347e67fef2d3f193449b152be5fb73
AlmogBaku <almog.baku@gmail.com>
2015-05-19 01:20:37 +0300

    - wizard check for data-roaming-switcher status - open settings

--- app/src/main/java/net/rimoto/wheninroam/activity/WizardActivity.java
+++ app/src/main/java/net/rimoto/wheninroam/activity/WizardActivity.java
@@ -1,27 +1,45 @@
 package net.rimoto.wheninroam.activity;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
-import android.os.Build;
+import android.content.Intent;
 import android.provider.Settings;
 import android.widget.TextView;
 
 import net.rimoto.wheninroam.R;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_wizard)
 public class WizardActivity extends Activity {
-
     @ViewById
-    protected TextView textView2;
+    protected TextView roamingSwitcherStatusView;
 
-    @Click(R.id.button2)
-    protected void onClickBtn() {
-        textView2.setText(Boolean.toString(isDataRoamingEnabled()));
+    @AfterViews
+    protected void afterViews() {
+        onClickRoamingDataBtn();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(roamingSwitcherStatusView !=null) {
+            onClickRoamingDataBtn();
+        }
+    }
+
+    @Click(R.id.roamingDataBtn)
+    protected void onClickRoamingDataBtn() {
+        roamingSwitcherStatusView.setText(Boolean.toString(isDataRoamingEnabled()));
+    }
+    @Click(R.id.openSettingsBtn)
+    protected void onClickOpenSettingsBtn() {
+        Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(android.provider.Settings.ACTION_DATA_ROAMING_SETTINGS);
+        startActivity(intent);
     }
 
     protected boolean isDataRoamingEnabled() {
