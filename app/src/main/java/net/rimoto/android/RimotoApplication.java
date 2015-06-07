4643595a62fc168dc747cdf2d3290d44901a9a37
AlmogBaku <almog.baku@gmail.com>
2015-06-07 17:54:55 +0300

    update to new oauth creds

--- app/src/main/java/net/rimoto/android/RimotoApplication.java
+++ app/src/main/java/net/rimoto/android/RimotoApplication.java
@@ -9,8 +9,8 @@ import org.androidannotations.annotations.EApplication;
 
 @EApplication
 public class RimotoApplication extends Application {
-    private final String rimotoClientID = "3_4xs1w1ynms08w8sc0w0k8k40kksccwwwwo8scso44c08c4goo4";
-    private final String rimoto_authURI = "http://wheninroam.parseapp.com/inbound_rimoto_auth";
+    private final String rimotoClientID = "1_56hxwnph188wkosoc8c8ccswskkwcwggc4k8o4k0gs4ksgsk08";
+    private final String rimoto_authURI = "http://localhost/";
 
     @Override
     public void onCreate() {
