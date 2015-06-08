eb091efe78b56a8f4ef48fd27d9d71d7f85b0853
AlmogBaku <almog.baku@gmail.com>
2015-06-08 22:42:40 +0300

    remove test btns from wizard

--- app/src/main/java/net/rimoto/android/fragment/WizardRoamingFragment.java
+++ app/src/main/java/net/rimoto/android/fragment/WizardRoamingFragment.java
@@ -13,20 +13,10 @@ import org.androidannotations.annotations.EFragment;
 @EFragment(R.layout.fragment_wizard_roaming)
 public class WizardRoamingFragment extends Fragment {
 
-    @Click
-    protected void openSettingsBtn() {
+    protected void openRoamingSettings() {
         Intent intent = new Intent();
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setAction(android.provider.Settings.ACTION_DATA_ROAMING_SETTINGS);
         startActivity(intent);
-    }
-
-
-
-    @Click
-    protected void gotoLoginBtn() {
-        Intent intent = new Intent(getActivity(), LoginActivity_.class);
-        startActivity(intent);
-        getActivity().finish();
     }
 }
