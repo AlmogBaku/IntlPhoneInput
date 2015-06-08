ce0842ed6811316da37f91e9139ac4a633f8d839
AlmogBaku <almog.baku@gmail.com>
2015-06-08 20:11:22 +0300

    "Main" GUI

--- app/src/main/java/net/rimoto/android/adapter/SCEServicesRecycleAdapter.java
+++ app/src/main/java/net/rimoto/android/adapter/SCEServicesRecycleAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.rimoto.android.R;
@@ -37,16 +38,27 @@ public class SCEServicesRecycleAdapter extends RecyclerView.Adapter<SCEServicesR
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView serviceName;
+        public ImageView serviceIcon;
         private SCEService service;
+        private static String packageName;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             serviceName = (TextView) itemView.findViewById(R.id.serviceName);
+            serviceIcon = (ImageView) itemView.findViewById(R.id.serviceIcon);
+
+            if(packageName==null) {
+                packageName = itemView.getContext().getPackageName();
+            }
         }
         public void setService(SCEService service) {
             this.service = service;
-            serviceName.setText(service.getName());
+            serviceName.setText(service.getName() + ":");
+            int id = itemView.getContext().getResources().getIdentifier("service_"+service.getSlug(), "drawable", packageName);
+            if(id!=0) {
+                serviceIcon.setImageResource(id);
+            }
         }
     }
 }
