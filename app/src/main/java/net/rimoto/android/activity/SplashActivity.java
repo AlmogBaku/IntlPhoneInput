16410e60d2981be88d2ad3e9c30498f6cba6d692
AlmogBaku <almog.baku@gmail.com>
2015-06-09 18:46:55 +0300

    fixes leaking

--- app/src/main/java/net/rimoto/android/activity/SplashActivity.java
+++ app/src/main/java/net/rimoto/android/activity/SplashActivity.java
@@ -14,27 +14,29 @@ import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_splash)
 public class SplashActivity extends Activity {
+    private static final int SPLASH_TIME_OUT = 1500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         VpnLog.getInstance().registerLogcat();
 
-        Intent intent;
-        if(Session.getCurrentAccessToken()==null) {
-            intent = new Intent(this, LoginActivity_.class);
-        } else {
-            if(VpnManager.isActive(this)) {
-                intent = new Intent(this, MainActivity_.class);
-            } else {
-                intent = new Intent(this, WizardActivity_.class);
-            }
-        }
-
         final Handler handler = new Handler();
         handler.postDelayed(() -> {
+            Intent intent;
+            if(Session.getCurrentAccessToken()==null) {
+                intent = new Intent(this, LoginActivity_.class);
+            } else {
+                if(VpnManager.isActive(this)) {
+                    intent = new Intent(this, MainActivity_.class);
+                } else {
+                    intent = new Intent(this, WizardActivity_.class);
+                }
+            }
+
             startActivity(intent);
             finish();
-        }, 2000);
+        }, SPLASH_TIME_OUT);
     }
 }
