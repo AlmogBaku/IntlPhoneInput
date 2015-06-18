89835750254b6be558be1bb11dcc4a226fddc953
AlmogBaku <almog.baku@gmail.com>
2015-06-18 16:16:14 +0300

    add spinner on connecting

--- app/src/main/java/net/rimoto/android/activity/WizardActivity.java
+++ app/src/main/java/net/rimoto/android/activity/WizardActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.instabug.wrapper.support.activity.InstabugFragmentActivity;
 import com.viewpagerindicator.CirclePageIndicator;
@@ -15,6 +16,7 @@ import net.rimoto.android.R;
 import net.rimoto.android.adapter.WizardFragmentAdapter;
 import net.rimoto.core.API;
 import net.rimoto.core.models.Policy;
+import net.rimoto.core.utils.UI;
 import net.rimoto.core.utils.VpnUtils;
 
 import org.androidannotations.annotations.AfterViews;
@@ -22,6 +24,7 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import de.blinkt.openvpn.core.VpnStatus;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -81,10 +84,36 @@ public class WizardActivity extends InstabugFragmentActivity {
     }
 
 
+    private class RimotoStateListener implements VpnStatus.StateListener {
+        @Override
+        public void updateState(String state, String logmessage, int localizedResId, VpnStatus.ConnectionStatus level) {
+            Log.d("vp", state);
+            if(state.equals("CONNECTED")) {
+                UI.hideSpinner();
+                startMainActivity();
+                this.finish();
+            } else if(state.equals("EXITING")) {
+                UI.hideSpinner();
+                Toast toast = Toast.makeText(WizardActivity.this, "There was some problem with connecting you :\\", Toast.LENGTH_LONG);
+                toast.show();
+                this.finish();
+            }
+        }
+
+        private void finish() {
+            VpnStatus.removeStateListener(this);
+            mStateListener=null;
+        }
+    }
+
+    private RimotoStateListener mStateListener = new RimotoStateListener();
+
     @Click
     protected void connectBtn() {
+        connectBtn.setEnabled(false);
+        UI.showSpinner(this, "Connecting to rimoto's cloud..");
         VpnUtils.startVPN(this);
-        startMainActivity();
+        VpnStatus.addStateListener(mStateListener);
     }
     private void startMainActivity() {
         Intent intent = new Intent(this, MainActivity_.class);
--- app/src/main/java/net/rimoto/core/utils/UI.java
+++ app/src/main/java/net/rimoto/core/utils/UI.java
@@ -6,9 +6,12 @@ import android.content.Context;
 public class UI {
     private static ProgressDialog sSpinner;
     public static void showSpinner(Context context) {
+        showSpinner(context, "Loading. Please wait...");
+    }
+    public static void showSpinner(Context context, String message) {
         sSpinner = new ProgressDialog(context);
         sSpinner.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        sSpinner.setMessage("Loading. Please wait...");
+        sSpinner.setMessage(message);
         sSpinner.setIndeterminate(true);
         sSpinner.setCanceledOnTouchOutside(false);
         sSpinner.show();
