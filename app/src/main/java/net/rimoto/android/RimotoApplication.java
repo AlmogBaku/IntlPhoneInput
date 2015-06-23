ad3094afc72e04be6de94495b94fb2279e2f6c94
AlmogBaku <almog.baku@gmail.com>
2015-06-24 01:30:57 +0300

    add leaking detect and detect

--- app/src/main/java/net/rimoto/android/RimotoApplication.java
+++ app/src/main/java/net/rimoto/android/RimotoApplication.java
@@ -1,9 +1,14 @@
 package net.rimoto.android;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.instabug.library.Instabug;
+import com.squareup.leakcanary.AndroidExcludedRefs;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
+import net.rimoto.android.utils.LeakSlackUploadService;
 import net.rimoto.core.RimotoCore;
 
 import org.androidannotations.annotations.EApplication;
@@ -16,9 +21,22 @@ public class RimotoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        refWatcher = installLeakCanary();
         Instabug.initialize(this, "cfad51965edc86000b61bd545c4d84d2");
 
         RimotoCore.init(this, rimotoClientID, rimoto_authURI);
         RimotoCore.setAuthType("token");
     }
+
+
+    public static RefWatcher getRefWatcher(Context context) {
+        RimotoApplication application = (RimotoApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
+    protected RefWatcher installLeakCanary() {
+        return LeakCanary.install(this, LeakSlackUploadService.class, AndroidExcludedRefs.createAppDefaults().build());
+    }
+
+    private RefWatcher refWatcher;
 }
---
+++ app/src/main/java/net/rimoto/android/utils/LeakSlackUploadService.java
@@ -0,0 +1,76 @@
+package net.rimoto.android.utils;
+
+import android.util.Log;
+import com.squareup.leakcanary.AnalysisResult;
+import com.squareup.leakcanary.DisplayLeakService;
+import com.squareup.leakcanary.HeapDump;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.http.Multipart;
+import retrofit.http.POST;
+import retrofit.http.Part;
+import retrofit.mime.TypedFile;
+
+public final class LeakSlackUploadService extends DisplayLeakService {
+
+    /** See https://api.slack.com/ for documentation. */
+    public interface SlackApi {
+        String TOKEN = "xoxb-6762386405-RgquQTgsXh5pGogThtkhO6RI";
+
+        String MEMORY_LEAK_CHANNEL = "C06MN9EAC";
+
+        @Multipart @POST("/api/files.upload") UploadFileResponse uploadFile(@Part("token") String token,
+                                                                            @Part("file") TypedFile file, @Part("filetype") String filetype,
+                                                                            @Part("filename") String filename, @Part("title") String title,
+                                                                            @Part("initial_comment") String initialComment, @Part("channels") String channels);
+    }
+
+    public static class UploadFileResponse {
+        boolean ok;
+        String error;
+
+        @Override public String toString() {
+            return "UploadFileResponse{" +
+                    "ok=" + ok +
+                    ", error='" + error + '\'' +
+                    '}';
+        }
+    }
+
+    private static final String TAG = "LeakListenerService";
+
+    private static String classSimpleName(String className) {
+        int separator = className.lastIndexOf('.');
+        return separator == -1 ? className : className.substring(separator + 1);
+    }
+
+    private SlackApi slackApi;
+
+    @Override public void onCreate() {
+        super.onCreate();
+        slackApi = new RestAdapter.Builder() //
+                .setEndpoint("https://slack.com") //
+                .build() //
+                .create(SlackApi.class);
+    }
+
+    @Override
+    protected void afterDefaultHandling(HeapDump heapDump, AnalysisResult result, String leakInfo) {
+        if (!result.leakFound || result.excludedLeak) {
+            return;
+        }
+        String name = classSimpleName(result.className);
+        if (!heapDump.referenceName.equals("")) {
+            name += "(" + heapDump.referenceName + ")";
+        }
+
+        String title = name + " has leaked";
+        try {
+            slackApi.uploadFile(SlackApi.TOKEN,
+                    new TypedFile("application/octet-stream", heapDump.heapDumpFile), null,
+                    heapDump.heapDumpFile.getName(), title, leakInfo, SlackApi.MEMORY_LEAK_CHANNEL);
+        } catch (RetrofitError e) {
+            Log.e(TAG, "Error when uploading heap dump", e);
+        }
+    }
+}
