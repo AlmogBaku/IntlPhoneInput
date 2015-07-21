a5dc789ec67c326abf852ef99caa8e3e93e906be
AlmogBaku <almog.baku@gmail.com>
2015-07-21 19:04:24 +0300

    new wizard design

--- app/src/main/java/net/rimoto/android/views/PacificoTextView.java
+++ app/src/main/java/net/rimoto/android/views/PacificoTextView.java
@@ -1,8 +1,10 @@
 package net.rimoto.android.views;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
@@ -20,5 +22,21 @@ public class PacificoTextView extends TextView {
 
     private void init() {
         this.setTypeface(Typeface.createFromAsset(this.getContext().getAssets(), "fonts/Pacifico-Regular.ttf"));
+        setIncludeFontPadding(false);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        super.setMeasuredDimension(getMeasuredWidth(), (int)(getBaseline()*1.3));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        float offset = (getBaseline() - getLineHeight())*0.5f;
+
+        canvas.translate(0, offset);
+        super.onDraw(canvas);
+
     }
 }
