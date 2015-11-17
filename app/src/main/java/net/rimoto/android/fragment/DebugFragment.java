2e502069b64310abcbe1e131fdbb275f07612b64
AlmogBaku <almog.baku@gmail.com>
2015-11-17 17:33:41 +0200

    Option to change wifiPolicy on the debug screen

--- app/src/main/java/net/rimoto/android/fragment/DebugFragment.java
+++ app/src/main/java/net/rimoto/android/fragment/DebugFragment.java
@@ -1,10 +1,15 @@
 package net.rimoto.android.fragment;
 
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import net.rimoto.android.R;
 import net.rimoto.core.Session;
+import net.rimoto.core.utils.VpnUtils;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
@@ -15,6 +20,9 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
+
+import de.blinkt.openvpn.VpnProfile;
+import de.blinkt.openvpn.core.ProfileManager;
 
 @EFragment(R.layout.fragment_debug)
 public class DebugFragment extends Fragment {
@@ -27,11 +35,36 @@ public class DebugFragment extends Fragment {
     @ViewById(R.id.internalIp)
     protected EditText ipEditTextView;
 
+    @ViewById(R.id.wifiPolicy)
+    protected Spinner wifiPolicySpinner;
+
+    private VpnProfile mVpnProfile;
+
     @AfterViews
     protected void afterViews() {
         accessTokenEditTextView.setText(Session.getCurrentAccessToken().getToken());
-        sidEditTextView.setText(""+Session.getCurrentSubscriber().getId());
+        sidEditTextView.setText("" + Session.getCurrentSubscriber().getId());
         ipEditTextView.setText(getIpAddress());
+
+        mVpnProfile = ProfileManager
+                .get(getActivity(), VpnUtils.getCurrentProfileUUID(getActivity()));
+
+        ArrayAdapter<VpnProfile.WifiPolicy> adapter = new ArrayAdapter<VpnProfile.WifiPolicy>(getActivity(),
+                android.R.layout.simple_spinner_item, VpnProfile.WifiPolicy.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        wifiPolicySpinner.setAdapter(adapter);
+        wifiPolicySpinner.setSelection(mVpnProfile.mWifiPolicy.ordinal());
+        wifiPolicySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mVpnProfile.mWifiPolicy = VpnProfile.WifiPolicy.values()[position];
+                ProfileManager.getInstance(getContext()).saveProfile(getContext(), mVpnProfile);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
     }
 
     private String getIpAddress() {
