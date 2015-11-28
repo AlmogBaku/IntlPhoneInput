e1700d2fd4f8b8a75a8f424344d6e68cbedefe20
AlmogBaku <almog.baku@gmail.com>
2015-11-28 23:58:06 +0200

    add server info to Instabug

--- app/src/main/java/net/rimoto/android/utils/InstabugRimoto.java
+++ app/src/main/java/net/rimoto/android/utils/InstabugRimoto.java
@@ -2,6 +2,7 @@ package net.rimoto.android.utils;
 
 import android.content.Context;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.instabug.library.Instabug;
@@ -12,8 +13,13 @@ import net.rimoto.core.models.Subscriber;
 import net.rimoto.core.utils.VpnUtils;
 import net.rimoto.vpnlib.VpnManager;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 
+import de.blinkt.openvpn.VpnProfile;
+import de.blinkt.openvpn.core.Connection;
+import de.blinkt.openvpn.core.ProfileManager;
 import de.blinkt.openvpn.core.VpnStatus;
 
 public class InstabugRimoto {
@@ -35,6 +41,18 @@ public class InstabugRimoto {
                                 +"vpn_connected: " + VpnManager.isConnected() + "\r\n"
                                 +"vpn_state: " + VpnStatus.getLaststate() + "\r\n"
                     ;
+
+            VpnProfile profile = ProfileManager.get(context,VpnUtils.getCurrentProfileUUID(context));
+            if(profile != null) {
+                List<String> connections = new ArrayList<String>();
+                for(Connection connection : profile.mConnections) {
+                    connections.add(connection.mServerName+":"+connection.mServerPort);
+                }
+                userData += "vpn_profile.servers: [ " + TextUtils.join(", ", connections) + " ]\r\n";
+                userData += "vpn_profile.wifi_policy: " + VpnProfile.WifiPolicy.values()[profile.mWifiPolicy.ordinal()] + " ]\r\n";
+                userData += "vpn_profile.roaming_policy: " + VpnProfile.RoamingPolicy.values()[profile.mRoamingPolicy.ordinal()] + " ]\r\n";
+            }
+
             Log.d("testfairy-correlation-id", String.valueOf(subscriber.getId()));
             Instabug.getInstance().setUserData(userData);
         });
