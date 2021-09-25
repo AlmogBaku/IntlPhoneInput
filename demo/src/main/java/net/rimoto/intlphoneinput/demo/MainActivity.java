84ae498df77959fe79ea994f252c7b3d5f85a4a7
Ashish <ashish@vikisoftware.com>
2021-09-25 21:35:23 +0530

    Project Update and migration
    1. Add support ti Android API 30
    2. Migrated project to androidx
    3. Update gradle version
    4. Replace jcenter with mavenCentral

--- demo/src/main/java/net/rimoto/intlphoneinput/demo/MainActivity.java
+++ demo/src/main/java/net/rimoto/intlphoneinput/demo/MainActivity.java
@@ -1,6 +1,6 @@
 package net.rimoto.intlphoneinput.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
